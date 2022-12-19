7a2ccecd7abebc4e8d8126b40959f5c1c11b31be
Drew Porter <dporter91@gmail.com>
2022-12-19 12:03:13 -0900

    added separate sheet replacing low values with detection limits

--- detection limit.R
+++ detection limit.R
@@ -1,4 +1,6 @@
 #These are the packages you need for this script use install.packages() if you don't have them
+
+library(NADA)
 library(NADA2)
 library(EnvStats)
 library(dplyr)
@@ -8,7 +10,14 @@ wd <- getwd()
 dir.data <- file.path(wd, "data")
 dir.output <- file.path(wd, "output")
 
-meta <- read.csv(file = file.path(dir.data, "Thiaminase.Meta.2022.csv"),
+# for working with censored data, I need the "Thiaminase Activity" values in
+# my spreadsheet to be either 1) the actual measured value, if it is above
+# the calculated detection limit, or 2) the calculated detection limit if the 
+# observed value was below that limit, so I am saving a second "metadata" csv
+# file where I replace all values below the detection limit, with the detection 
+# limit
+
+meta <- read.csv(file = file.path(dir.data, "Thiaminase.Meta.2022.detection.limit.csv"),
                  stringsAsFactors = TRUE,
                  blank.lines.skip = TRUE)
 
@@ -19,26 +28,30 @@ meta <- meta[-c(1:9),] #removing samples that were not analysed
 #what is the % of nondetects observed for each species?
 meta %>% 
   group_by(Species) %>% 
-  summarise(n = n(), Pct_Nondetect = ((sum(No_detect == TRUE))/n()) * 100)
+  summarise(n = n(), Pct_Nondetect = ((sum(No_detect == TRUE))/n()) * 100,
+            nDetect = n() - sum(No_detect))
 
 # Don't bother with due to small sample size and high incidence of nondetects:
   # Greenland turbot, Humpy shrimp, Longhead dab, Pacific cod (age 0), 
   # Threespine stickelback
+# Fourhorn Sculpin have fewer than 3 observations above the detection limit so I
+# am removing them too
 
 #removing the above species in a new dataframe
 meta_trimmed_species <- subset(meta, !(Species %in% c("Greenland turbot",
                                                       "Humpy shrimp", 
                                                       "Longhead Dab",
                                                       "Pacific cod (age 0)",
-                                                      "Threespine stickleback")))
+                                                      "Threespine stickleback",
+                                                      "Fourhorn Sculpin")))
+# remove unused factor levels in Species
+meta_trimmed_species$Species <- droplevels(meta_trimmed_species$Species)
+
 meta_trimmed_species %>% 
   group_by(Species) %>% 
   summarise(n = n(), Pct_Nondetect = ((sum(No_detect == TRUE))/n()) * 100)
 
 #need to separate the species
-
-unique(meta$Species)
-
 #make a list with the species separated
 separate_species <- meta %>% 
   group_split(Species)
@@ -50,11 +63,14 @@ ArcticCisco <- separate_species[[1]] # n = 9
 ArcticCod <- separate_species[[2]] # n = 10
 BroadWhitefish <- separate_species[[3]] # n = 9
 Capelin <- separate_species[[4]] # n = 11
-FourhornSculpin <- separate_species[[5]] # n = 10
+#FourhornSculpin <- separate_species[[5]] # n = 10
 GonatusSquid <- separate_species[[6]] # n = 6
 PacificHerring <- separate_species[[11]] # n = 28
 PacificHerringAge0 <- separate_species[[12]] # n = 8
+
 RainbowSmelt <- separate_species[[13]] # n = 22
+RainbowSmelt$Survey <- droplevels(RainbowSmelt$Survey) #remove unused factor levels
+
 SaffronCod <- separate_species[[14]] # n = 14
 SandLance <- separate_species[[15]] # n = 18
 SurfSmelt <- separate_species[[16]] # n = 11
@@ -70,5 +86,26 @@ ggplot(data = meta_trimmed_species, aes(x = Mass..g., y = Thiaminase_Activity)) 
   
 
 
+?cboxplot
+cboxplot(meta_trimmed_species$Thiaminase_Activity, meta_trimmed_species$No_detect,
+         xgroup = meta_trimmed_species$Species, show = TRUE)
+
+cboxplot(Capelin$Thiaminase_Activity, Capelin$No_detect, xgroup = Capelin$Survey)
+
+xx = cboxplot(RainbowSmelt$Thiaminase_Activity, RainbowSmelt$No_detect, xgroup = RainbowSmelt$Survey)
+
+censtats(RainbowSmelt$Thiaminase_Activity, RainbowSmelt$No_detect)
+?censtats
+censtats(SandLance$Thiaminase_Activity, SandLance$No_detect)
+?cenros
 
 
+unique(RainbowSmelt$Survey)
+SandLanceNBS <- SandLance[which(SandLance$Survey == c("NBS2021", "NBS2022")),]
+
+RainbowSmeltNBS <- RainbowSmelt[which(RainbowSmelt$Survey == c("NBS2021", "NBS2022")),]
+RainbowSmeltNBS <- RainbowSmelt[which(RainbowSmelt$Survey == c("NBS2021", "NBS2022")),]
+
+censtats(SandLanceNBS2021$Thiaminase_Activity, San)
+
+?which
